e0842c6cd92bde5067821ea5d643db5ca29ea230
peppermint100 <krpeppermint100@gmail.com>
2024-10-15 14:24:55 +0900

    spring-cloud-gateway를 통한 라우팅

---
+++ apigateway-service/src/main/java/com/peppermint100/apigatewayservice/ApigatewayServiceApplication.java
@@ -0,0 +1,14 @@
+package com.peppermint100.apigatewayservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class ApigatewayServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApigatewayServiceApplication.class, args);
+    }
+
+}
---
+++ second-service/src/test/java/com/peppermint100/secondservice/SecondServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.peppermint100.secondservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecondServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
