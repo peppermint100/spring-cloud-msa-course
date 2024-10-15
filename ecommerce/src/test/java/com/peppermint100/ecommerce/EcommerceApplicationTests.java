904ec7137076bffb1b8634c1e428e3bc0f181945
peppermint100 <krpeppermint100@gmail.com>
2024-10-15 13:41:02 +0900

    init

---
+++ ecommerce/src/test/java/com/peppermint100/ecommerce/EcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.peppermint100.ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ user-service/src/test/java/com/peppermint100/user_service/UserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.peppermint100.user_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
