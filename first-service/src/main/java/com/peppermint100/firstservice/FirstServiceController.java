30678adc212583e0f3b70dd0492d982f5a533428
peppermint100 <krpeppermint100@gmail.com>
2024-10-15 14:53:37 +0900

    yml, java code를 통한 필터 적용

--- first-service/src/main/java/com/peppermint100/firstservice/FirstServiceController.java
+++ first-service/src/main/java/com/peppermint100/firstservice/FirstServiceController.java
@@ -1,6 +1,7 @@
 package com.peppermint100.firstservice;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,5 +12,11 @@ public class FirstServiceController {
     @GetMapping("/welcome")
     public String welcome() {
         return "Welcome to first service";
+    }
+
+    @GetMapping("/message")
+    public String message(@RequestHeader("first-request") String firstRequestHeader) {
+        System.out.println(firstRequestHeader);
+        return "First Service Message";
     }
 }
--- second-service/src/main/java/com/peppermint100/secondservice/SecondServiceController.java
+++ second-service/src/main/java/com/peppermint100/secondservice/SecondServiceController.java
@@ -1,6 +1,7 @@
 package com.peppermint100.secondservice;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,5 +12,11 @@ public class SecondServiceController {
     @GetMapping("/welcome")
     public String welcome() {
         return "Welcome to second service";
+    }
+
+    @GetMapping("/message")
+    public String message(@RequestHeader("second-request") String secondRequestHeader) {
+        System.out.println(secondRequestHeader);
+        return "Second Service Message";
     }
 }
