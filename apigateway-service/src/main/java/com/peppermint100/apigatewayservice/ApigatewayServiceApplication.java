eb9ea3cb92b9e606b98442c6639a0b9b75dc8bef
peppermint100 <krpeppermint100@gmail.com>
2024-10-17 22:54:06 +0900

    Git repository로 Config 등록, RabbitMQ를 사용한 Config BusRefresh

--- apigateway-service/src/main/java/com/peppermint100/apigatewayservice/ApigatewayServiceApplication.java
+++ apigateway-service/src/main/java/com/peppermint100/apigatewayservice/ApigatewayServiceApplication.java
@@ -1,7 +1,10 @@
 package com.peppermint100.apigatewayservice;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.web.exchanges.HttpExchangeRepository;
+import org.springframework.boot.actuate.web.exchanges.InMemoryHttpExchangeRepository;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
@@ -11,4 +14,8 @@ public class ApigatewayServiceApplication {
         SpringApplication.run(ApigatewayServiceApplication.class, args);
     }
 
+    @Bean
+    public HttpExchangeRepository httpExchangeRepository() {
+        return new InMemoryHttpExchangeRepository();
+    }
 }
